aa44163eb44d9cbc33e36eb879ae0e457368d5b6
ebrahim <viroos@live.com>
2022-07-22 22:00:37 +0430

    Revert "v1 printer12aa"
    This reverts commit 780fea6cb791b31ca41e671695c5c7b2fdcc1fb2.

--- src/main/java/service/Main.java
+++ src/main/java/service/Main.java
@@ -7,6 +7,22 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
+
+//        ApplicationConstant.getUserRepository().createUserTable();
+//
+//       User u1 = new User(-1,"hosein","sadeqi","zohress","1234");
+//       ApplicationConstant.getUserRepository().createUser(u1);
+
+        // List<User> return_user = ApplicationConstant.getUserRepository().findAllUser();
+        //  System.out.println(return_user);
+
+//        User user = ApplicationConstant.getUserRepository().findUserById(4);
+//        System.out.println(user);
+//        user.setfName("ali");
+//        user.setlName("nouri");
+//        ApplicationConstant.getUserRepository().updateUser(4,user);
         ApplicationConstant.getUserMenu().runFirstMenu();
     }
+
+
 }
--- src/main/java/service/menu/UserMenu.java
+++ src/main/java/service/menu/UserMenu.java
@@ -1,15 +1,10 @@
 package service.menu;
 
-import entity.User;
-import repository.UserRepository;
 import service.ApplicationConstant;
 import service.Printer;
 
-import javax.swing.plaf.IconUIResource;
-import java.sql.SQLException;
-
 public class UserMenu {
-    public void runFirstMenu() throws SQLException {
+    public void runFirstMenu() {
         System.out.println("welcome");
         while (true) {
             Printer.print(ApplicationConstant.USER_MENU);
@@ -20,8 +15,8 @@ public class UserMenu {
                 case 1:
                     login();
                     break;
-                case 2:
-                    signup();
+                case 2 :
+                    signin();
                     break;
                 case 3:
                     System.exit(1);
@@ -32,54 +27,13 @@ public class UserMenu {
         }
     }
 
-    public void login() throws SQLException {
-        User user = new User();
-        System.out.print("Enter a username : ");
-        String inputUsername = ApplicationConstant.getInput().next();
-        System.out.print("Enter a password : ");
-        String inputPassword = ApplicationConstant.getInput().next();
-
-        if(ApplicationConstant.getUserRepository().isUsernameExist(inputUsername)){
-            //user = ApplicationConstant.getUserRepository().UserByUsername(inputUsername);
-            // ok it
-            if(user.getPassword().equals(inputPassword)){
-                System.out.println("welcome " + user.getfName());
-
-            }
-        }else {
-            System.out.println("username or password is not correct");
-        }
-
-
+    public void login() {
 
     }
 
-    public void signup() throws SQLException {
-        User user = new User();
-        String username;
-        while (true) {
-            System.out.println("Enter username : ");
-            username = ApplicationConstant.getInput().next();
-            if (ApplicationConstant.getUserRepository().isUsernameExist(username)) {
-                continue;
-            } else {
-                user.setUserName(username);
+    public void signin() {
 
-                break;
-            }
-        }
-        System.out.println("Enter firstname : ");
-        String firstname = ApplicationConstant.getInput().next();
-        user.setfName(firstname);
-
-        System.out.println("Enter lastname : ");
-        String lastname = ApplicationConstant.getInput().next();
-        user.setlName(lastname);
-
-        System.out.println("Enter password : ");
-        String password = ApplicationConstant.getInput().next();
-        user.setPassword(password);
-
-        ApplicationConstant.getUserRepository().createUser(user);
     }
+
+
 }
